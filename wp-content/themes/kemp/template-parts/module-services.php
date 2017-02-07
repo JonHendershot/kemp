45c9551e19e356acc5039f7f94e04dee304547e0
Jon Hendershot <jon@staygrind.com>
2017-02-07 18:41:30 -0500

    fix vh bug

--- wp-content/themes/kemp/template-parts/module-services.php
+++ wp-content/themes/kemp/template-parts/module-services.php
@@ -12,7 +12,7 @@
 	if ($ii % 2 == 0) { $color_key = 'white'; } else { $color_key = 'black'; } 
 ?>
 
-<div class="color-trigger service vh <?php echo strtolower( get_the_title() ); if(! $image[0]){ echo ' noclip'; } ?>" style="background-image: url(<?php echo $image[0]; ?>)" data-color-key="<?php echo $color_key; ?>">
+<div class="color-trigger service <?php if($ii <= 3){echo 'vh '; } echo strtolower( get_the_title() ); if(! $image[0]){ echo ' noclip'; } ?>" style="background-image: url(<?php echo $image[0]; ?>)" data-color-key="<?php echo $color_key; ?>">
 	<div class="service-wrapper">
 		<h2 class="<?php if($image[0]){ echo "clip-text"; } ?> service-title" <?php if($image[0]){ echo "style='background-image: url(" . $image[0] . ")';"; } ?>> <?php the_title(); ?></h2>
 		<div class="<?php if($image[0]){ echo "clip-text"; } ?> service-content" <?php if($image[0]){ echo "style='background-image: url(" . $image[0] . ")';"; } ?>><?php echo $line_1; if($line_2){echo "<br />$line_2"; }?></div>
